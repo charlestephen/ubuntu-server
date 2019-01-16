5e0cdc376a2cfda1f1a68ba40ea27469380c2b23
Charles Stephen Thompson <charles@brooklynhelpdesk.com>
2019-01-15 22:46:08 -0500

    v1.16

--- Home Automation/Homebridge/setup_modules.sh
+++
@@ -1,8 +0,0 @@
-#installs the following homebridge modules:
-#openweathermap
-#unifi-occupancy-sensor
-#bluetooth
-#homeassistant
-#bluetooth
-#miio device discovery
-docker exec -it homebridge npm install -g homebridge-openweathermap homebridge-unifi-occupancy-sensor miio homebridge-homeassistant
